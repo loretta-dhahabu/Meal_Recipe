1d870c0666968cd0182533f75851f48f7d57cabd
loretta dhahabu <loreydjefwa@gmail.com>
2023-04-05 10:41:02 +0300

    declare variables for the search creteria

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const baseUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s";
+
+const mealImage = document.querySelector("#meal-image");
+const mealIngredient = document.querySelector("#ingredients");
+const mealRecipe = document.querySelector("#recipe");
+const mealName = document.querySelector("#meal-name");
+const mealList = document.querySelector("#meal-list");
+const searchMealButton = document.querySelector("#search-meal");
+const input = document.querySelector("#input");
+const searchMealForm = document.querySelector("#search-meal-form");
+const searchCriteria = document.querySelectorAll(".criterion");
